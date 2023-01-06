69d315c13592e7c45ee3847e1d681d753fec7a6e
Dmytro Bevzenko <dbvz@protonmail.com>
2023-01-05 22:27:59 -0600

    Better names for tests

--- src/transpose/exercise.test.js
+++ src/transpose/exercise.test.js
@@ -4,13 +4,13 @@ describe("Transpose", () => {
   test("empty matrix", () => {
     expect(transpose([])).toEqual([]);
   });
-  test("single row matrix transposes single column matrix", () => {
+  test("single row matrix (2x1) transposes single column matrix (1x2)", () => {
     expect(transpose(["A1"])).toEqual(["A", "1"]);
   });
-  test("single column matrix transposes single row matrix", () => {
+  test("single column matrix (1x2) transposes single row matrix (2x1)", () => {
     expect(transpose(["A", "1"])).toEqual(["A1"]);
   });
-  test("3x2 matrix", () => {
+  test("3x2 transposes into 2x3", () => {
     const input = ["ABC", "123"];
     const expected = ["A1", "B2", "C3"];
     expect(transpose(input)).toEqual(expected);
