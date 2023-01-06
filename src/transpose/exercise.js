b12b23362f52fc847b661d36641c13bc890c5fe6
Dmytro Bevzenko <dbvz@protonmail.com>
2023-01-05 22:37:19 -0600

    Refactoring
    Refactored `transpose` function. Introduced additional helper function that transpose single row and single column.

--- src/transpose/exercise.js
+++ src/transpose/exercise.js
@@ -3,21 +3,33 @@
 // convenience to get you started writing code faster.
 //
 
+function transposeColumn(input) {
+  const output = input.join("");
+  return [output];
+}
+
+function transposeRow(input) {
+  const output = input[0].split("");
+  return output;
+}
+
+function isSingleColumn(input) {
+  return input.length === 2;
+}
+
 function transpose(input) {
   // It satisfies the case if input = []
   if (input.length === 0) {
     return input;
   }
+
   // satisfies input = ['A', '1']
-  if (input.length === 2) {
-    const output = input.join("");
-    return [output];
+  if (isSingleColumn(input)) {
+    return transposeColumn(input);
   }
-  //
 
   // satisfies input = ['A1']
-  const output = input[0].split("");
-  return output;
-};
+  return transposeRow(input);
+}
 
 module.exports = transpose;
